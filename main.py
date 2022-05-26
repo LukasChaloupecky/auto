6b36b0f147e85271e471a92672d45dcf782f9842
LukasChaloupecky <91658013+LukasChaloupecky@users.noreply.github.com>
2022-05-26 11:33:21 +0200

    pro ctverec

--- main.py
+++ main.py
@@ -5,6 +5,10 @@
 #u křižovatek- naprogramovat přes ovladání možnost > až uvidíž křižovatku > ("odboč vlevo, nebo vpravo")
 
 
+
+# udelat z rychlosti 600 100 atd. promene pres radio (pres druhy microbit pak pujde ovladat otaceni- podle toho jak moc bude draha kulata ci rovna)
+# 
+count = 0     #dostává vozítka do správné trajektorie aby potom mohl odbocin vpravo nebo vlevo 
 moznost = ""
 radio.set_group(25)
 def turnright():
@@ -26,7 +30,7 @@ def forward():
     pins.digital_write_pin(DigitalPin.P12, 1)
 def turnleft():
     global item
-    pins.analog_write_pin(AnalogPin.P1, 200)
+    pins.analog_write_pin(AnalogPin.P1, 100)
     pins.digital_write_pin(DigitalPin.P8, 1)
     pins.analog_write_pin(AnalogPin.P2, 600)
     pins.digital_write_pin(DigitalPin.P12, 1)
@@ -36,14 +40,34 @@ led.enable(False)
 pins.set_pull(DigitalPin.P4, PinPullMode.PULL_UP)
 pins.set_pull(DigitalPin.P5, PinPullMode.PULL_UP)
 item = 0
-
+def ctverecVeSmeru():
+    forward_ = 800 #rychlost u ctverce 
+    rightMesni = 100
+    rightVetsi = 800
+    leftMensi = 300
+    leftVetsi = 600
+    right_Mensi = 300
+    right_Vetsi = 600
+    
+    if pins.digital_read_pin(DigitalPin.P4) == 0 and pins.digital_read_pin(DigitalPin.P5) == 0:
+        turnright()
+    if pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P5) == 1:
+        forward()
+    if pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P5) == 0:
+        turnleft()
+    if pins.digital_read_pin(DigitalPin.P4) == 0 and pins.digital_read_pin(DigitalPin.P5) == 1:
+        pass #turnright_uvnitr()dodělat 
 def on_forever():
     global item, moznost
     
     if moznost == "krizovatkaVLEVO" and pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P5) == 1:
         turnleft()
     elif moznost == "krizovatkaVPRAVO" and pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P5) == 1:
-        turnright() 
+        if count == 0:
+            pins.analog_write_pin(AnalogPin.P1, 0) #levý motor
+            pins.analog_write_pin(AnalogPin.P2, 600) #pravy motor
+            basic.pause(250) #doba otaceni do praveho uhlu
+            
     elif pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P5) == 1:
         forward()
 
@@ -70,4 +94,15 @@ basic.forever(on_forever)
 def on_received_string(receivedString):
     global moznost
     moznost = receivedString
+    if receivedString == "CtverecVeSmeruHodinovychRucicek":
+        ctverecVeSmeru()
 radio.on_received_string(on_received_string)
+
+
+
+
+
+
+
+
+
--- main.ts
+++ main.ts
@@ -3,6 +3,10 @@
 // levý senzor->
 // pravý sensor->
 // u křižovatek- naprogramovat přes ovladání možnost > až uvidíž křižovatku > ("odboč vlevo, nebo vpravo")
+//  udelat z rychlosti 600 100 atd. promene pres radio (pres druhy microbit pak pujde ovladat otaceni- podle toho jak moc bude draha kulata ci rovna)
+//  
+let count = 0
+// dostává vozítka do správné trajektorie aby potom mohl odbocin vpravo nebo vlevo 
 let moznost = ""
 radio.setGroup(25)
 function turnright() {
@@ -30,7 +34,7 @@ function forward() {
 
 function turnleft() {
     
-    pins.analogWritePin(AnalogPin.P1, 200)
+    pins.analogWritePin(AnalogPin.P1, 100)
     pins.digitalWritePin(DigitalPin.P8, 1)
     pins.analogWritePin(AnalogPin.P2, 600)
     pins.digitalWritePin(DigitalPin.P12, 1)
@@ -42,13 +46,49 @@ led.enable(false)
 pins.setPull(DigitalPin.P4, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
 item = 0
+function ctverecVeSmeru() {
+    let forward_ = 800
+    // rychlost u ctverce 
+    let rightMesni = 100
+    let rightVetsi = 800
+    let leftMensi = 300
+    let leftVetsi = 600
+    let right_Mensi = 300
+    let right_Vetsi = 600
+    if (pins.digitalReadPin(DigitalPin.P4) == 0 && pins.digitalReadPin(DigitalPin.P5) == 0) {
+        turnright()
+    }
+    
+    if (pins.digitalReadPin(DigitalPin.P4) == 1 && pins.digitalReadPin(DigitalPin.P5) == 1) {
+        forward()
+    }
+    
+    if (pins.digitalReadPin(DigitalPin.P4) == 1 && pins.digitalReadPin(DigitalPin.P5) == 0) {
+        turnleft()
+    }
+    
+    if (pins.digitalReadPin(DigitalPin.P4) == 0 && pins.digitalReadPin(DigitalPin.P5) == 1) {
+        
+    }
+    
+}
+
+// turnright_uvnitr()dodělat 
 basic.forever(function on_forever() {
     
     if (moznost == "krizovatkaVLEVO" && pins.digitalReadPin(DigitalPin.P4) == 1 && pins.digitalReadPin(DigitalPin.P5) == 1) {
         turnleft()
     } else if (moznost == "krizovatkaVPRAVO" && pins.digitalReadPin(DigitalPin.P4) == 1 && pins.digitalReadPin(DigitalPin.P5) == 1) {
-        turnright()
+        if (count == 0) {
+            pins.analogWritePin(AnalogPin.P1, 0)
+            // levý motor
+            pins.analogWritePin(AnalogPin.P2, 600)
+            // pravy motor
+            basic.pause(250)
+        }
+        
     } else if (pins.digitalReadPin(DigitalPin.P4) == 1 && pins.digitalReadPin(DigitalPin.P5) == 1) {
+        // doba otaceni do praveho uhlu
         forward()
     } else if (pins.digitalReadPin(DigitalPin.P4) == 0 && pins.digitalReadPin(DigitalPin.P5) == 0) {
         item += 1
@@ -76,4 +116,8 @@ basic.forever(function on_forever() {
 radio.onReceivedString(function on_received_string(receivedString: string) {
     
     moznost = receivedString
+    if (receivedString == "CtverecVeSmeruHodinovychRucicek") {
+        ctverecVeSmeru()
+    }
+    
 })
