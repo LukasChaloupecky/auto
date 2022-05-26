f14013983bce401d68b50c43d8105d6630812d8f
LukasChaloupecky <91658013+LukasChaloupecky@users.noreply.github.com>
2022-05-26 10:42:32 +0200

    zadaání a primituivní kod

---
+++ main.py
@@ -0,0 +1,51 @@
+#levý motor->
+#pravý motor->
+#levý senzor->
+#pravý sensor->
+#u křižovatek- naprogramovat přes ovladání možnost > až uvidíž křižovatku > ("odboč vlevo, nebo vpravo")
+
+
+
+def turnright():
+    global item
+    pins.analog_write_pin(AnalogPin.P1, 600)
+    pins.digital_write_pin(DigitalPin.P8, 1)
+    pins.analog_write_pin(AnalogPin.P2, 100)
+    pins.digital_write_pin(DigitalPin.P12, 1)
+    item = 0
+def stop():
+    pins.analog_write_pin(AnalogPin.P1, 0)
+    pins.digital_write_pin(DigitalPin.P8, 0)
+    pins.analog_write_pin(AnalogPin.P2, 0)
+    pins.digital_write_pin(DigitalPin.P12, 0)
+def forward():
+    pins.analog_write_pin(AnalogPin.P1, 600)
+    pins.analog_write_pin(AnalogPin.P2, 600)
+    pins.digital_write_pin(DigitalPin.P8, 1)
+    pins.digital_write_pin(DigitalPin.P12, 1)
+def turnleft():
+    global item
+    pins.analog_write_pin(AnalogPin.P1, 100)
+    pins.digital_write_pin(DigitalPin.P8, 1)
+    pins.analog_write_pin(AnalogPin.P2, 600)
+    pins.digital_write_pin(DigitalPin.P12, 1)
+    item = 0
+item = 0
+led.enable(False)
+pins.set_pull(DigitalPin.P4, PinPullMode.PULL_UP)
+pins.set_pull(DigitalPin.P5, PinPullMode.PULL_UP)
+item = 0
+
+def on_forever():
+    global item
+    if pins.digital_read_pin(DigitalPin.P4) == 0 and pins.digital_read_pin(DigitalPin.P5) == 0:
+        forward()
+    elif pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P5) == 1:
+        item += 1
+    elif pins.digital_read_pin(DigitalPin.P4) == 1 and pins.digital_read_pin(DigitalPin.P5) == 0:
+        turnright()
+    elif pins.digital_read_pin(DigitalPin.P4) == 0 and pins.digital_read_pin(DigitalPin.P5) == 1:
+        turnleft()
+    if item > 30:
+        stop()
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,59 @@
+// levý motor->
+// pravý motor->
+// levý senzor->
+// pravý sensor->
+// u křižovatek- naprogramovat přes ovladání možnost > až uvidíž křižovatku > ("odboč vlevo, nebo vpravo")
+function turnright() {
+    
+    pins.analogWritePin(AnalogPin.P1, 600)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.analogWritePin(AnalogPin.P2, 100)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    item = 0
+}
 
+function stop() {
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+}
+
+function forward() {
+    pins.analogWritePin(AnalogPin.P1, 600)
+    pins.analogWritePin(AnalogPin.P2, 600)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+}
+
+function turnleft() {
+    
+    pins.analogWritePin(AnalogPin.P1, 100)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    pins.analogWritePin(AnalogPin.P2, 600)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    item = 0
+}
+
+let item = 0
+led.enable(false)
+pins.setPull(DigitalPin.P4, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
+item = 0
+basic.forever(function on_forever() {
+    
+    if (pins.digitalReadPin(DigitalPin.P4) == 0 && pins.digitalReadPin(DigitalPin.P5) == 0) {
+        forward()
+    } else if (pins.digitalReadPin(DigitalPin.P4) == 1 && pins.digitalReadPin(DigitalPin.P5) == 1) {
+        item += 1
+    } else if (pins.digitalReadPin(DigitalPin.P4) == 1 && pins.digitalReadPin(DigitalPin.P5) == 0) {
+        turnright()
+    } else if (pins.digitalReadPin(DigitalPin.P4) == 0 && pins.digitalReadPin(DigitalPin.P5) == 1) {
+        turnleft()
+    }
+    
+    if (item > 30) {
+        stop()
+    }
+    
+})
